e57bed4d7a9dc8999b963a4a10175f0b09b3149f
Andrey <volk000@gmail.com>
2023-10-30 14:38:29 +0100

    Add migration to add init db data

---
+++ queue_manager/default_db_data.py
@@ -0,0 +1,27 @@
+class DefaultDBData:
+    groups = {
+        'SUPERVISORS': 'supervisors',
+        'OPERATORS': 'operators',
+    }
+
+    default_supervisor = {
+        'username': 'default_sepervisor',
+        'password': 'default_pass',
+        'first_name': 'Default',
+        'last_name': 'Supervisor',
+    }
+
+    permissions_for_supervisors = (
+        'add_operator',
+        'change_operator',
+        'view_operator',
+        'delete_operator',
+        'add_task',
+        'change_task',
+        'view_task',
+        'delete_task',
+        'add_session',
+        'change_session',
+        'view_session',
+        'view_ticket',
+    )
