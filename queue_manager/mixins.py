79776a44c37bd6952262883fe537e9f6c3a8d7ef
Andrey <volk000@gmail.com>
2023-12-22 15:08:52 +0300

    Refactor TopNavMenuMixin

--- queue_manager/mixins.py
+++ queue_manager/mixins.py
@@ -6,78 +6,76 @@ from queue_manager.user.models import Operator
 
 class ContextMixinWithItemName(ContextMixin):
     def get_context_data(self, **kwargs):
-        '''Get ItemName to use in universal Add, Upd, Del templates'''
+        '''Get ItemName to use in universal Add, Upd, Del templates.'''
         context = super().get_context_data(**kwargs)
         context['item_name'] = self.item_name
         return context
 
 
-class TopNavMenuMixin(ContextMixin):  # TODO Refactor
+class TopNavMenuMixin(ContextMixin):
     def get_context_data(self, **kwargs):  # noqa C901
-        '''Returns items for navigation menu at the top of any page'''
+        '''Creates items for navigation menu at the top of any page.
+
+        Returns: list of items (tuples of name and link)'''
+
         context = super().get_context_data(**kwargs)
 
         menu_items = []
-        path = self.request.path
+        path = self.request.path.split('/')[1:]
+        print(f'=== PATH: {path} ===')
 
-        if path != '/':
+        if len(path[0]) > 0:
             menu_items.append(('Home', reverse_lazy('home')))
 
-        if path.startswith('/client/') and len(path) > len('/client/'):
-            menu_items.append(('Client', reverse_lazy('print-ticket')))
+            if path[0] == 'client':
+                if path[1]:
+                    menu_items.append(('Client', reverse_lazy('print-ticket')))
 
-        elif path.startswith('/supervisor/'):
-            pass
-
-        elif path.startswith('/task/'):
-            menu_items.append(
-                ('Supervisor', reverse_lazy('supervisor-enter')))
-
-        elif path.startswith('/session/'):
-            menu_items.append(
-                ('Supervisor', reverse_lazy('supervisor-enter')))
-
-        elif path.startswith('/operator/'):
-            if 'select' in path or 'no_permission' in path:
+            elif path[0] == 'supervisor':
                 pass
-            elif 'manage' in path:
+
+            elif path[0] in ('task', 'session'):
                 menu_items.append(
                     ('Supervisor', reverse_lazy('supervisor-enter')))
-            else:
-                request_by_supervisor = self.request.user.groups.filter(
-                        name=DefaultDBData.groups['SUPERVISORS']).exists()
-                if request_by_supervisor and len(path) > len('/operator/'):
-                    pretend_id = int(path.split('/')[2])
-                    menu_items.append(('Operator',
-                                       reverse_lazy('operator-enter')))
-                    menu_items.append((
-                        Operator.objects.get(id=pretend_id).get_full_name(),
-                        reverse_lazy('operator-personal',
-                                     kwargs={'pk': pretend_id})
-                        ))
+
+            elif path[0] == 'operator':
+                if path[1] in ('select', 'no_permission'):
+                    pass
+                elif path[1] == 'manage':
+                    menu_items.append(
+                        ('Supervisor', reverse_lazy('supervisor-enter')))
                 else:
                     menu_items.append(
                         ('Operator', reverse_lazy('operator-enter')))
-
-        elif path.startswith('/ticket/'):
-            if self.request.GET.get('operator'):
-                menu_items.append(('Operator',
-                                   reverse_lazy('operator-enter')))
-
-                pretend_operator_id = self.request.GET.get('operator')
-                request_by_supervisor = self.request.user.groups.filter(
+                    request_by_supervisor = self.request.user.groups.filter(
                             name=DefaultDBData.groups['SUPERVISORS']).exists()
-                if pretend_operator_id and request_by_supervisor:
-                    menu_items.append((
-                        Operator.objects.get(
-                                id=int(pretend_operator_id)).get_full_name(),
-                        reverse_lazy('operator-personal',
-                                     kwargs={'pk': int(pretend_operator_id)})
-                        ))
+                    if request_by_supervisor:
+                        pretend_id = int(path[1])
+                        menu_items.append((
+                            Operator.objects.get(id=pretend_id)
+                                    .get_full_name(),
+                            reverse_lazy('operator-personal',
+                                         kwargs={'pk': pretend_id})))
 
-            else:
-                menu_items.append(
-                    ('Supervisor', reverse_lazy('supervisor-enter')))
+            elif path[0] == 'ticket':
+                pretend_operator_id = self.request.GET.get('operator')
+                if pretend_operator_id:
+                    menu_items.append(('Operator',
+                                      reverse_lazy('operator-enter')))
+
+                    request_by_supervisor = self.request.user.groups.filter(
+                            name=DefaultDBData.groups['SUPERVISORS']).exists()
+                    if request_by_supervisor:
+                        pretend_id = int(pretend_operator_id)
+                        menu_items.append((
+                            Operator.objects.get(
+                                id=pretend_id).get_full_name(),
+                            reverse_lazy('operator-personal',
+                                         kwargs={'pk': pretend_id})))
+
+                else:
+                    menu_items.append(
+                        ('Supervisor', reverse_lazy('supervisor-enter')))
 
         context['top_nav_menu'] = menu_items
         return context
