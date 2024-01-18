d784ed9309b72103b0b242615332c4e17086f983
Andrey <volk000@gmail.com>
2024-01-18 17:47:00 +0100

    Remove duplicat in Operator create/upd form

--- queue_manager/user/forms.py
+++ queue_manager/user/forms.py
@@ -5,7 +5,20 @@ from django.contrib.auth.forms import UserCreationForm, SetPasswordForm
 from django import forms
 
 
-class OperatorCreateForm(UserCreationForm):
+class ModelFormWithTaskSet(forms.ModelForm):
+    task_set = forms.ModelMultipleChoiceField(
+        label="Can serve tasks",
+        widget=forms.CheckboxSelectMultiple(),
+        queryset=Task.objects.all().order_by('letter_code'),
+        required=False)
+
+    def save(self, *args, **kwargs):
+        instance = super().save(commit=True)
+        instance.task_set.set(self.cleaned_data['task_set'])
+        return instance
+
+
+class OperatorCreateForm(UserCreationForm, ModelFormWithTaskSet):
     class Meta:
         model = Operator
         fields = [
@@ -17,19 +30,8 @@ class OperatorCreateForm(UserCreationForm):
             'task_set',
             ]
 
-    task_set = forms.ModelMultipleChoiceField(
-        label="Can serve tasks",
-        widget=forms.CheckboxSelectMultiple(),
-        queryset=Task.objects.all().order_by('letter_code'),
-        required=False)
 
-    def save(self, *args, **kwargs):
-        instance = super().save(*args, **kwargs)
-        instance.task_set.set(self.cleaned_data['task_set'])
-        return instance
-
-
-class OperatorUpdateForm(forms.ModelForm):
+class OperatorUpdateForm(ModelFormWithTaskSet):
     class Meta:
         model = Operator
         fields = [
@@ -39,21 +41,10 @@ class OperatorUpdateForm(forms.ModelForm):
             'task_set'
             ]
 
-    task_set = forms.ModelMultipleChoiceField(
-        label="Can serve tasks",
-        widget=forms.CheckboxSelectMultiple(),
-        queryset=Task.objects.all().order_by('letter_code'),
-        required=False)
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['task_set'].initial = self.instance.task_set.all(
             ).values_list('id', flat=True)
-
-    def save(self, *args, **kwargs):
-        instance = super().save(*args, **kwargs)
-        instance.task_set.set(self.cleaned_data['task_set'])
-        return instance
 
 
 class OperatorChangePasswordForm(SetPasswordForm):
